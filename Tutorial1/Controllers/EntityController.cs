9cfd8ce1897542fd6d901bdeff0aef54c9e1f719
Norton Pengra <npengra317@gmail.com>
2016-07-11 14:38:16 -0700

    Create Entity controller

---
+++ Tutorial1/Controllers/EntityController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Tutorial1.Models;
+
+namespace Tutorial1.Controllers
+{
+    public class EntityController : ApiController
+    {
+        Entity[] entities = new Entity[]
+        {
+            new Entity { id=1, name="Norton Pengra", age=19, health=100 },
+            new Entity { id=2, name="Oliver Collins", age=19, health=200 },
+            new Entity { id=3, name="PengraBot", age=1, health=2000 },
+        };
+
+        public IEnumerable<Entity> getAllEntities()
+        {
+            return entities;
+        }
+
+        public IHttpActionResult getEntity(int id)
+        {
+            Entity entity = entities.FirstOrDefault((e) => e.id == id);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            return Ok(entity);
+        }
+    }
+}
