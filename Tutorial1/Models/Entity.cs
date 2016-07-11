cbd8a0a412d754c3d47388611f5b0767ae75ed5b
Norton Pengra <npengra317@gmail.com>
2016-07-11 14:26:19 -0700

    Define Entity object

---
+++ Tutorial1/Models/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tutorial1.Models
+{
+    public class Entity
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public int health { get; set; }
+    }
+}
